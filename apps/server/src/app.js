661983c53b335dc47ddcf86c1c0cd15673a45b4c
Marek Zelinka <mzelinka17@gmail.com>
2026-04-26 11:23:35 +0200

    setup middlewares

--- apps/server/src/app.js
+++ apps/server/src/app.js
@@ -1,9 +1,23 @@
+import cors from "cors";
 import express from "express";
+import helmet from "helmet";
+import morgan from "morgan";
 
+import { middleware } from "./core/middleware.js";
 import { notesRouter } from "./routers/notes.js";
 
 export const app = express();
 
+app.use(helmet());
+app.use(cors());
+app.use(morgan("dev"));
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/api/health", (_req, res) => res.json({ status: "ok" }));
 
 app.use("/api/notes", notesRouter);
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware._MUST_BE_LAST_errorHandler);
